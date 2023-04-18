8ae329519386efa43866e134f1f1f77a630be06a
tormAtt <attetorma@gmail.com>
2023-04-18 12:29:14 +0300

    Kauppa luokka

--- pythonLuokat/pelaaja.py
+++ pythonLuokat/pelaaja.py
@@ -23,15 +23,39 @@ class Pelaaja():
 
     def SetRaha(self, raha):
         self.raha = raha
+        '''
+        sql = "UPDATE Game SET Money = '" + str(raha) + "' WHERE Game_ID = '" + str(
+        PeliID) + "'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        '''
 
     def SetPolttoAine(self, polttoAine):
         self.polttoaine = polttoAine
+        '''
+        sql = "UPDATE Game SET Fuel = '" + str(polttoAine) + "' WHERE Game_ID = '" + str(
+        PeliID) + "'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        '''
 
     def SetSotilaat(self, sotilaat):
         self.sotilaat = sotilaat
+        '''
+        sql = "UPDATE Game SET Troops = '" + str(sotilaat) + "' WHERE Game_ID = '" + str(
+        PeliID) + "'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        '''
 
     def SetScore(self, score):
         self.score = score
+        '''
+        uusiScore = float(GetScore()) + 100
+        sql = "UPDATE Game SET Score = '" + str(uusiScore) + "' WHERE Game_ID = '" + str(PeliID) + "'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        '''
 
     def SetSijainti(self, sijainti):
         self.sijainti = sijainti
