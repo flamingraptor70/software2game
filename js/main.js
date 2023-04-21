77f61a0e4b853f7c17bd637d27806893fb90a6ee
tormAtt <attetorma@gmail.com>
2023-04-21 13:32:14 +0300

    Työstetty python luokkia ja katottu miten voi yhdistää front endin ja back endin

--- js/main.js
+++ js/main.js
@@ -1,13 +1,35 @@
 'use strict'
 
-const form = document.getElementById("nameForm")
+const nForm = document.getElementById("nameForm");
+const paikatDiv = document.getElementById("paikatDiv");
 
 async function newGame(evt) {
-  evt.preventDefault()
-  const nimi =document.querySelector("input[name=nimi]").value
-  const response = await fetch("http://127.0.0.1:5000/newGame/" + nimi)
-  const jsonData = await response.json()
-  console.log(jsonData)
+  evt.preventDefault();
+  const nimi =document.querySelector("input[name=nimi]").value;
+  const response = await fetch("http://127.0.0.1:5000/newGame/" + nimi);
+  const jsonData = await response.json();
+  console.log(jsonData);
+  tulostaPaikat(jsonData);
 }
 
-form.addEventListener("submit", newGame)
+function tulostaPaikat(jsonData) {
+  for(let i = 0; i < jsonData.length; i++) {
+      const article = document.createElement("article");
+      const header = document.createElement("h2");
+      const hText = document.createTextNode(jsonData[i].nimi);
+      header.appendChild(hText)
+      const maaText = document.createTextNode("Maa: " + jsonData[i].maa + " ");
+      const icao = document.createTextNode("Icao: " + jsonData[i].icao);
+
+      const p = document.createElement("p");
+      p.appendChild(maaText);
+      p.appendChild(icao);
+
+      article.appendChild(header)
+      article.appendChild(p)
+
+    paikatDiv.appendChild(article)
+  }
+}
+
+nForm.addEventListener("submit", newGame);
