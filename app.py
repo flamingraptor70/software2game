6c73f8ff42ae0ecd6ff13663626d7426e311040f
tormAtt <attetorma@gmail.com>
2023-04-21 10:07:33 +0300

    Työstetty python luokkia

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from flask import Flask, request
 from flask_cors import CORS
 from peli import Peli
 
+app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "content-type"
+
 peli = Peli("Atte")
 
 peli.ArvoPaikat()
